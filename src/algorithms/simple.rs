d9b82db53cefd039fa66d0bbaf80d3454508114c
David Poole <davpoole@iastate.edu>
2020-09-25 16:17:13 -0500

    Changes to Monte-Carlo algorithm to save memory

--- src/algorithms/simple.rs
+++ src/algorithms/simple.rs
@@ -2,6 +2,28 @@ use rand::Rng;
 use rayon::prelude::*;
 
 pub fn compute(num_samples: u128) -> f64 {
+    let mut pi = 0.0;
+
+    for _iter in (1..=num_samples).into_iter() {
+      pi += 4.0
+        * ((1_u128..1_000_000_u128) 
+            .into_par_iter()
+            .map(|_| {
+                rand::thread_rng().gen_range(0.0_f64, 1.0_f64).powf(2.0)
+                    + rand::thread_rng().gen_range(0.0_f64, 1.0_f64).powf(2.0)
+                    <= 1.0
+            })
+            .into_par_iter()
+            .filter(|x| *x)
+            .collect::<Vec<bool>>()
+            .len() as f64)
+        / (num_samples as f64);
+    }
+    
+    return pi/(1_000_000 as f64);
+}
+/*
+pub fn compute(num_samples: u128) -> f64 {
     return 4.0
         * ((1_u128..(num_samples + 1_u128))
             .into_par_iter()
@@ -16,7 +38,7 @@ pub fn compute(num_samples: u128) -> f64 {
             .len() as f64)
         / (num_samples as f64);
 }
-
+*/
 /*
 pub fn compute<'a, T>(num_samples: T) -> f64
 where
